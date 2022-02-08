4956f78f85aaea1d6436a2350f0119440c83ca1f
AspergerKiD <66377201+rushilmohan@users.noreply.github.com>
2022-02-08 19:33:11 +0530

    Add files via upload

---
+++ scraping.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup as bs
+from matplotlib.pyplot import table
+import requests
+import pandas as pd
+
+url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
+
+page = requests.get(url)
+#print(page)
+
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find('table')
+
+temp_list = []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+Star_names = []
+Distance = []
+Mass = []
+Radius = []
+Lum = []
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    Mass.append(temp_list[i][5])
+    Radius.append(temp_list[i][6])
+    Lum.append(temp_list[i][7])
+
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_Name','Distance','Mass','Radius','Luminosity'])
+#print(df2)
+
+df2.to_csv('bright_star.csv')
